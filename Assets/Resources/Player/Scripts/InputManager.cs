3fd5cc29563ff5dff5a0004084fb4e291b48294b
MoritaSuguru <morisugood@gmail.com>
2016-12-14 09:20:33 +0900

    【入力関係の機能を追加・他】
    ・入力関係の機能をもったInputManagerクラスを作成しました。
    ・※現在はスワイプのみ反応します。
    ・その他、プレイヤーが使用するキャラクターのディレクトリ構成を一部変更しました。

---
+++ Assets/Resources/Player/Scripts/InputManager.cs
@@ -0,0 +1,148 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// クラス名：森田　勝
+/// 作成日　：H28/11/19
+/// 概要　　：入力の状態を扱うクラス
+///			　タップ、スワイプ、長押しといった機能を扱うクラスを
+///			　使い分けてます。
+/// </summary>
+public class InputManager : MonoBehaviour {
+
+	/// <summary>
+	/// Singleton
+	/// </summary>
+	private static InputManager instance;
+	
+	public static InputManager Instance {
+		get {
+			if(instance == null) {
+				instance = new GameObject("InputManager").AddComponent<InputManager>();
+			}
+			return instance;
+		}
+	}
+
+	public enum State {
+		Up,		//指が上がっている
+		Down	//指が下がっている（入力中）
+	}
+
+	enum InputType {
+		Free,	//何もしていない
+		Tap,	//タップ
+		Press,	//長押し
+		Swipe	//スワイプ
+	}
+
+	private InputType m_currentType;
+
+	//入力経過時間
+	private float m_elapsedTime;
+	//入力の開始座標
+	private Vector3 m_startPosition;
+	//入力の終了座標
+	private Vector3 m_endPosition;
+	//入力開始座標から移動した距離
+	private float m_inputLength;
+	//指が下がっている（入力中か）
+	private bool m_isDown;
+
+	private State m_currentState;
+
+	public State CurrentState {
+		get { return m_currentState; }
+	}
+
+	//現在、指しているターゲット
+	private GameObject m_currentTarget;
+	// Use this for initialization
+	void Start () {
+		m_currentState = State.Up;
+		m_currentType = InputType.Free;
+	}
+
+	// Update is called once per frame
+	void Update() {
+
+		if (Input.GetMouseButtonDown(0)) {	
+			BeginInput();
+		}
+		if (Input.GetMouseButton(0)) {
+			ReadInput();
+		}
+		if(Input.GetMouseButtonUp(0)) {
+			EndInput();
+		}
+
+	}
+
+	public bool Tap(GameObject target) {
+		bool result =
+			(m_currentTarget == target) &&
+			(m_currentType == InputType.Tap);
+
+		return result;
+	}
+
+	public bool Swipe(GameObject target) {
+		bool result = 
+			(m_currentTarget == target) && 
+			(m_currentType == InputType.Swipe);
+
+		return result;
+	}
+
+	public bool Press(GameObject target) {
+		bool result =
+					(m_currentTarget == target) &&
+					(m_currentType == InputType.Swipe);
+
+		return result;
+	}
+
+	private void BeginInput() {
+		m_currentState = State.Down;
+		m_startPosition = Input.mousePosition;
+		m_isDown = true;
+		m_elapsedTime = 0;
+		m_inputLength = 0;
+
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit hitInfo;
+		if(Physics.Raycast(ray,out hitInfo)) {
+			m_currentTarget = hitInfo.collider.gameObject;
+		}
+		else {
+			m_currentTarget = null;
+		}
+	}
+
+	private void ReadInput() {
+		
+		m_endPosition = Input.mousePosition;
+		m_elapsedTime += Time.deltaTime;
+		m_inputLength = Vector3.Distance(m_startPosition , m_endPosition);
+
+		if(m_elapsedTime < 1.0f) {
+			//入力してから一定時間まではタップとみなす
+			m_currentType = InputType.Tap;
+		}
+		else if(m_inputLength > 100) {
+			//入力してから一定距離指を動かすとスワイプとみなす
+			m_currentType = InputType.Swipe;
+		}
+		else {
+			//一定時間以上指を動かさずいると長押しとみなす
+			//※すでにスワイプとみなされている場合は除く
+			if(m_currentType != InputType.Swipe)
+			m_currentType = InputType.Press;
+		}
+	}
+
+	private void EndInput() {
+		m_currentState = State.Up;
+		
+	}
+}
---
+++ Assets/Resources/Player/Scripts/LineMaker.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LineMaker : MonoBehaviour {
+
+	public enum Phase {
+		Free,       //何もしてない
+		Making,    //描いている
+		Completion, //完成
+	}
+
+	private Vector2[] linePositions;
+
+	private const int MAX_LINE_OBJECT = 100;
+	public int max {
+		get { return MAX_LINE_OBJECT; }
+	}
+	private int pointNum = 0;
+	public int count {
+		get { return pointNum; }
+	}
+	private Vector3 mousePos;
+
+	private LineRenderer lineRenderer;
+
+	Vector2 beforePos;
+	[SerializeField]
+	public bool moveOk;
+	// Use this for initialization
+	void Start() {
+		linePositions = new Vector2[MAX_LINE_OBJECT];
+		lineRenderer = GetComponent<LineRenderer>();
+		moveOk = false;
+	}
+
+	// Update is called once per frame
+	void Update() {
+		
+	}
+}
